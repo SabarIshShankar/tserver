12946704d7e03823f8d7353da28c8c954d13fd01
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-09-18 05:40:13 +0000

    models

--- routes.js
+++ routes.js
@@ -15,3 +15,35 @@ router.post('/authenticate', users.validate, users.authenticate);
 router.param('post', posts.load);
 router.post('/posts', [require, posts.validate], post.create);
 router.get('/post/:post', posts.show);
+router.get('/posts', post.list);
+router.get('/posts/:category', posts.listByCategory);
+router,get('/user/:username', posts.listByUser);
+router.delete('/post/:post', [requireAuth, postAuth], posts.delete);
+
+router.get('/post/:post/upvote', requireAith, votes.upvote);
+router.get('/post/:post/downvote', requireAuth, votes.downvote);
+router.get('/post/:post/unvote', requireAuth, votes.unvote);
+
+router.param('comment', comments.load);
+router.post('/post/:post', [requireAuth, comments.validate], 
+comments.create);
+router.delete(
+	'/post/:post/:comment',
+	[requireAuth, commentAuth],
+	comments.delete
+);
+
+module.exports = (app) => {
+	app.use('/api', router);
+	app.use((req, res, next) => {
+		const error = new Error('Not found');
+		error.status = 404;
+		next(error);
+	});
+
+	app.use((error, req, res, next) => {
+		res.status(error.status || 500).json({
+			message: error.message
+		});
+	});
+};
